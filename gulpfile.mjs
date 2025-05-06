96f4b11ccb27ca2966f1339f3c6c3e3669e74074
linh <linh@canopylab.com>
2025-05-06 12:19:18 +0700

    implement footer, header, intro, enrich sections

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,10 +4,18 @@ import pug from 'gulp-pug';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
-import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
 import sourcemaps from 'gulp-sourcemaps';
 import clean from 'gulp-clean';
+import concat from 'gulp-concat';
+import terser from 'gulp-terser';
+import cleanCSS from 'gulp-clean-css';
+
+
+import fse from 'fs-extra';
+import * as glob from 'glob';
+import path from 'path';
+
 
 const sass = gulpSass(dartSass);
 const server = browserSync.create();
@@ -15,8 +23,21 @@ const server = browserSync.create();
 const paths = {
   html: './src/**/*.html',
   scss: './src/scss/**/*.scss',
+  vendorsCss: [
+    'src/css/vendors/bootstrap.min.css'
+  ],
+  pluginsCss: [
+    'src/css/plugins/jquery-ui.min.css'
+  ],
+  vendors: [
+    'src/js/vendors/jquery-3.7.1.min.js',
+    'src/js/vendors/bootstrap.bundle.min.js'
+  ],
+  plugins: [
+    'src/js/plugins/jquery-ui.min.js'
+  ],
   js: './src/js/**/*.js',
-  img: './src/img/**/*',
+  img: './src/img/**/*.{jpg,jpeg,png,gif,svg,webp}',
   dist: './dist',
 };
 
@@ -45,9 +66,53 @@ export const styles = () =>
     .pipe(gulp.dest(`${paths.dist}/css`))
     .pipe(server.stream());
 
+
+// Vendors CSS
+export const vendorsCSS = () =>
+  gulp.src(paths.vendorsCss)
+    .pipe(concat('vendors.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(`${paths.dist}/css`));
+
+// Plugins CSS
+export const pluginsCSS = () =>
+  gulp.src(paths.pluginsCss)
+    .pipe(concat('plugins.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(`${paths.dist}/css`));
+
 // Images
-export const images = () =>
-  gulp.src(paths.img).pipe(imagemin()).pipe(gulp.dest(`${paths.dist}/img`));
+// export const images = () =>
+//   gulp.src(paths.img, { buffer: true }).pipe(gulp.dest(`${paths.dist}/img`));
+
+export const copyImages = async () => {
+  const files = glob.sync('src/img/**/*.{png,jpg,jpeg,gif,svg,webp}');
+  await Promise.all(
+    files.map(async (file) => {
+      const destPath = file.replace('src/img', 'dist/img');
+      await fse.ensureDir(path.dirname(destPath));
+      await fse.copy(file, destPath);
+    })
+  );
+};
+
+// Vendors task
+export const vendors = () =>
+  gulp.src(paths.vendors)
+    .pipe(sourcemaps.init())
+    .pipe(concat('vendors.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(`${paths.dist}/js`));
+
+// Plugins task
+export const plugins = () =>
+  gulp.src(paths.plugins)
+    .pipe(sourcemaps.init())
+    .pipe(concat('plugins.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(`${paths.dist}/js`));
 
 // JS
 export const scripts = () => gulp.src(paths.js).pipe(gulp.dest(`${paths.dist}/js`));
@@ -67,11 +132,17 @@ export const serve = () => {
 
   gulp.watch(paths.html, html).on('change', server.reload);
   gulp.watch('src/templates/**/*.pug', compilePug).on('change', server.reload);
+  gulp.watch('src/js/vendors/**/*.js', vendors).on('change', server.reload);
+  gulp.watch('src/js/plugins/**/*.js', plugins).on('change', server.reload);
   gulp.watch(paths.scss, styles);
+  gulp.watch('src/css/vendors/**/*.css', vendorsCSS).on('change', server.reload);
+  gulp.watch('src/css/plugins/**/*.css', pluginsCSS).on('change', server.reload);
   gulp.watch(paths.js, scripts).on('change', server.reload);
-  gulp.watch(paths.img, images).on('change', server.reload);
+  // gulp.watch(paths.img, images).on('change', server.reload);
+  gulp.watch('src/img/**/*', copyImages).on('change', server.reload);
+
 };
 
-export const build = gulp.series(cleanDist, gulp.parallel(html, compilePug, styles, scripts, images));
+export const build = gulp.series(cleanDist, gulp.parallel(html, compilePug, styles, vendors, plugins, scripts, copyImages, vendorsCSS, pluginsCSS));
 
 export default gulp.series(build, serve);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,28 @@
+$(function () {
+  $(".datepicker")
+    .datepicker({
+      dateFormat: "D, M d, yy",
+      altField: function () {
+        return $(this).next(".datepicker-alt");
+      },
+      altFormat: "yy-mm-dd",
+    })
+    .datepicker("setDate", new Date());
 
-window.$ = window.jQuery = require('jquery');
-import 'slick-carousel';
+  $(".datepicker").each(function () {
+    const $input = $(this);
+    const $icon = $input.siblings(".icon-updown");
 
+    $input.on("focus", function () {
+      $icon.addClass("icon-updown__open");
+    });
+
+    $input.on("blur", function () {
+      setTimeout(() => {
+        if (!$input.is(":focus")) {
+          $icon.removeClass("icon-updown__open");
+        }
+      }, 100);
+    });
+  });
+});
